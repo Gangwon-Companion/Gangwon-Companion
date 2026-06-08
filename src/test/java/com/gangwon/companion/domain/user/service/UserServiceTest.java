235f244a1f0ae110c2a0a201eb27419f1ed95a52
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-06-08 09:19:30 +0900

    test: UserService 단위 테스트

---
+++ src/test/java/com/gangwon/companion/domain/user/service/UserServiceTest.java
@@ -0,0 +1,121 @@
+package com.gangwon.companion.domain.user.service;
+
+import com.gangwon.companion.domain.user.dto.LoginRequest;
+import com.gangwon.companion.domain.user.dto.SignUpRequest;
+import com.gangwon.companion.domain.user.entity.User;
+import com.gangwon.companion.domain.user.repository.UserRepository;
+import com.gangwon.companion.global.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock UserRepository userRepository;
+    @Mock PasswordEncoder passwordEncoder;
+    @Mock JwtTokenProvider jwtTokenProvider;
+    @Mock AuthenticationManager authenticationManager;
+
+    @InjectMocks UserService userService;
+
+    @Test
+    void 회원가입_성공() {
+        SignUpRequest request = mock(SignUpRequest.class);
+        given(request.getUsername()).willReturn("testuser1");
+        given(request.getEmail()).willReturn("test@test.com");
+        given(request.getNickname()).willReturn("테스터");
+        given(request.getPassword()).willReturn("Test1234!");
+        given(userRepository.existsByUsername("testuser1")).willReturn(false);
+        given(userRepository.existsByEmail("test@test.com")).willReturn(false);
+        given(userRepository.existsByNickname("테스터")).willReturn(false);
+        given(passwordEncoder.encode(any())).willReturn("encodedPassword");
+
+        assertThatCode(() -> userService.signUp(request)).doesNotThrowAnyException();
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void 회원가입_중복_아이디_예외() {
+        SignUpRequest request = mock(SignUpRequest.class);
+        given(request.getUsername()).willReturn("testuser1");
+        given(userRepository.existsByUsername("testuser1")).willReturn(true);
+
+        assertThatThrownBy(() -> userService.signUp(request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 사용 중인 아이디입니다.");
+    }
+
+    @Test
+    void 회원가입_중복_이메일_예외() {
+        SignUpRequest request = mock(SignUpRequest.class);
+        given(request.getUsername()).willReturn("testuser1");
+        given(request.getEmail()).willReturn("test@test.com");
+        given(userRepository.existsByUsername("testuser1")).willReturn(false);
+        given(userRepository.existsByEmail("test@test.com")).willReturn(true);
+
+        assertThatThrownBy(() -> userService.signUp(request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 사용 중인 이메일입니다.");
+    }
+
+    @Test
+    void 회원가입_중복_닉네임_예외() {
+        SignUpRequest request = mock(SignUpRequest.class);
+        given(request.getUsername()).willReturn("testuser1");
+        given(request.getEmail()).willReturn("test@test.com");
+        given(request.getNickname()).willReturn("테스터");
+        given(userRepository.existsByUsername("testuser1")).willReturn(false);
+        given(userRepository.existsByEmail("test@test.com")).willReturn(false);
+        given(userRepository.existsByNickname("테스터")).willReturn(true);
+
+        assertThatThrownBy(() -> userService.signUp(request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 사용 중인 닉네임입니다.");
+    }
+
+    @Test
+    void 로그인_성공() {
+        LoginRequest request = mock(LoginRequest.class);
+        given(request.getUsername()).willReturn("testuser1");
+        given(request.getPassword()).willReturn("Test1234!");
+
+        Authentication authentication = mock(Authentication.class);
+        given(authentication.getName()).willReturn("testuser1");
+        given(authenticationManager.authenticate(any())).willReturn(authentication);
+        given(jwtTokenProvider.generateToken("testuser1")).willReturn("mock.jwt.token");
+
+        String token = userService.login(request);
+
+        assertThat(token).isEqualTo("mock.jwt.token");
+    }
+
+    @Test
+    void 아이디_존재여부_확인() {
+        given(userRepository.existsByUsername("testuser1")).willReturn(true);
+        assertThat(userService.existsByUsername("testuser1")).isTrue();
+
+        given(userRepository.existsByUsername("newuser")).willReturn(false);
+        assertThat(userService.existsByUsername("newuser")).isFalse();
+    }
+
+    @Test
+    void 닉네임_존재여부_확인() {
+        given(userRepository.existsByNickname("테스터")).willReturn(true);
+        assertThat(userService.existsByNickname("테스터")).isTrue();
+
+        given(userRepository.existsByNickname("새닉")).willReturn(false);
+        assertThat(userService.existsByNickname("새닉")).isFalse();
+    }
+}
