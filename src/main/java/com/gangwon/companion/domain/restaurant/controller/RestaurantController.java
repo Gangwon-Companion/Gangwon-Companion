216df0c14c0023b5d05fa0eaf657ea834fad6153
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-06-15 22:59:13 +0900

    feat: 음식점 경로 조회 API 제거

--- src/main/java/com/gangwon/companion/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/gangwon/companion/domain/restaurant/controller/RestaurantController.java
@@ -4,7 +4,6 @@ import com.gangwon.companion.domain.restaurant.dto.RestaurantDetailResponse;
 import com.gangwon.companion.domain.restaurant.dto.RestaurantListResponse;
 import com.gangwon.companion.domain.restaurant.query.RestaurantSearchCriteria;
 import com.gangwon.companion.domain.restaurant.service.RestaurantService;
-import com.gangwon.companion.global.route.RouteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,13 +29,5 @@ public class RestaurantController {
     @GetMapping("/{restaurantId}")
     public ResponseEntity<RestaurantDetailResponse> getRestaurantDetail(@PathVariable Long restaurantId) {
         return ResponseEntity.ok(restaurantService.getRestaurantDetail(restaurantId));
-    }
-
-    @GetMapping("/{restaurantId}/route")
-    public ResponseEntity<RouteResponse> getRoute(
-            @PathVariable Long restaurantId,
-            @RequestParam Double userLat,
-            @RequestParam Double userLng) {
-        return ResponseEntity.ok(restaurantService.getRoute(restaurantId, userLat, userLng));
     }
 }
