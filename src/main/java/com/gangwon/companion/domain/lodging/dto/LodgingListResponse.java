0e77f5fbe5616d190de1da0db03f2d242ecda597
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-06-18 08:39:54 +0900

    feat : 숙소 dto 추가

---
+++ src/main/java/com/gangwon/companion/domain/lodging/dto/LodgingListResponse.java
@@ -0,0 +1,12 @@
+package com.gangwon.companion.domain.lodging.dto;
+
+import com.gangwon.companion.global.web.AbstractListResponse;
+
+import java.util.List;
+
+public class LodgingListResponse extends AbstractListResponse<LodgingItemResponse> {
+
+    public LodgingListResponse(long totalCount, List<LodgingItemResponse> items) {
+        super(totalCount, items);
+    }
+}
