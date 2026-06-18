00b99d8aa99d56e1ace3ed1c07fa24d79de4aed8
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-06-18 13:05:41 +0900

    feat : 조회 기능 추가

--- src/main/java/com/gangwon/companion/domain/lodging/repository/LodgingReviewRepository.java
+++ src/main/java/com/gangwon/companion/domain/lodging/repository/LodgingReviewRepository.java
@@ -4,8 +4,11 @@ import com.gangwon.companion.domain.lodging.entity.LodgingReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LodgingReviewRepository extends JpaRepository<LodgingReview, Long> {
 
     List<LodgingReview> findByLodgingId(Long lodgingId);
+
+    Optional<LodgingReview> findByIdAndLodgingId(Long id, Long lodgingId);
 }
--- src/main/java/com/gangwon/companion/domain/restaurant/repository/RestaurantReviewRepository.java
+++ src/main/java/com/gangwon/companion/domain/restaurant/repository/RestaurantReviewRepository.java
@@ -4,8 +4,11 @@ import com.gangwon.companion.domain.restaurant.entity.RestaurantReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantReviewRepository extends JpaRepository<RestaurantReview, Long> {
 
     List<RestaurantReview> findByRestaurantId(Long restaurantId);
+
+    Optional<RestaurantReview> findByIdAndRestaurantId(Long id, Long restaurantId);
 }
