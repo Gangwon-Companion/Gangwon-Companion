0fe0f37845f7afd6ef0a45d16c0f9d129599a7a9
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-06-28 19:30:45 +0900

    fix : 버그 수정

--- src/test/java/com/gangwon/companion/CompanionApplicationTests.java
+++ src/test/java/com/gangwon/companion/CompanionApplicationTests.java
@@ -3,9 +3,11 @@ package com.gangwon.companion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = "jwt.secret=gangwon_test_jwt_secret_key_2024_very_long_string")
 class CompanionApplicationTests {
 
 	@Test
