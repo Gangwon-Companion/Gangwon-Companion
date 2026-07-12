ca5a043ee4cf7c08ad95e1fd1c92e11a6d3ea7ed
KIM WANSU <poj5928@gmail.com>
2026-07-12 21:19:14 +0900

    fix: TourApiClient 빈 이름 충돌 해결

--- src/main/java/com/gangwon/companion/domain/home/client/TourApiClient.java
+++ src/main/java/com/gangwon/companion/domain/home/client/TourApiClient.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Component("homeTourApiClient")
 @RequiredArgsConstructor
 public class TourApiClient {
 
