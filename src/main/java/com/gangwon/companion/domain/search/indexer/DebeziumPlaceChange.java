c2974295159e048d7bf3b3f244f34b18873abc78
BakHyeongwo <toplionby421@gmail.com>
2026-08-28 12:21:50 +0900

    feat : 검색 색인 이벤트에 원본 발생 시각 추가

--- src/main/java/com/gangwon/companion/domain/search/indexer/DebeziumPlaceChange.java
+++ src/main/java/com/gangwon/companion/domain/search/indexer/DebeziumPlaceChange.java
@@ -1,6 +1,6 @@
 package com.gangwon.companion.domain.search.indexer;
 
-public record DebeziumPlaceChange(String domain, long id, boolean rootDeleted) {
+public record DebeziumPlaceChange(String domain, long id, boolean rootDeleted, long sourceTimestampMillis) {
     public String placeId() {
         return domain + ":" + id;
     }
