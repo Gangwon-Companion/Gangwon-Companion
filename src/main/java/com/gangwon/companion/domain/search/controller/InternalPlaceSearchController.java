58d07d722bccea5fe489bc9f0af508882feddafc
BakHyeongwo <toplionby421@gmail.com>
2026-09-07 22:34:06 +0900

    feat: 내부 장소 검색 근거 정보 반환 추가

--- src/main/java/com/gangwon/companion/domain/search/controller/InternalPlaceSearchController.java
+++ src/main/java/com/gangwon/companion/domain/search/controller/InternalPlaceSearchController.java
@@ -7,8 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +20,13 @@ public class InternalPlaceSearchController {
     private final PlaceSearchEngine searchEngine;
 
     @PostMapping("/places")
-    public ResponseEntity<PlaceSearchResponse> search(@RequestBody PlaceSearchRequest request) {
-        return ResponseEntity.ok(searchEngine.search(request));
+    public ResponseEntity<?> search(
+            @RequestBody PlaceSearchRequest request,
+            @RequestHeader(value = "X-Grounding-Trace", defaultValue = "false") boolean includeTrace
+    ) {
+        PlaceSearchResponse response = searchEngine.search(request);
+        return ResponseEntity.ok(includeTrace
+                ? response
+                : Map.of("results", response.results()));
     }
 }
